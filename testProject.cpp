d61de5b46b5240f118cb4be091af018887646833
Ewan McDonald <646067@student.cavc.ac.uk>
2019-09-05 12:52:42 +0100

    Commit 1

---
+++ testProject.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//Stops the need to add std before each statement
+using namespace std;
+
+//Program starts and ends here, however it can pull other functions out of the main() loop
+int main() {
+	helloWorld();
+
+
+}
+
+int helloWorld() {
+	cout << "Hello World";
+}
